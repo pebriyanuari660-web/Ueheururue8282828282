4ad83bd2d539540cad1a53572c8e412efc19fc57
pebriyanuari660-web <pebriyanuari660@gmail.com>
2026-05-25 01:14:48 +0700

    Update download URL in a1b2c3x8.js

--- a1b2c3x8.js
+++ a1b2c3x8.js
@@ -1,3 +1,2 @@
-export const downloadUrl =
-"https://github.com/pebriyanuari660-web/Ueheururue8282828282/releases/download/6272/controlmp4.7z";
-
+// Isi di dalam file config_a78x92.js
+export const downloadUrl = "https://github.com/pebriyanuari660-web/Ue8ehrrurhrurhrururu/releases/download/822882/videomp4.7z";
