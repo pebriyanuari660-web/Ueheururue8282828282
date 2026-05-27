9da5b8f771aa0675ddcdfd44c48faddb60b22eab
pebriyanuari660-web <pebriyanuari660@gmail.com>
2026-05-27 19:59:57 +0700

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -1,1 +1,67 @@
+export default async function handler(
+req,
+res
+){
 
+  const text =
+  req.body.message?.text || "";
+
+  // KIRIM
+  if(
+    text.toLowerCase() ===
+    "kirim"
+  ){
+
+    await fetch(
+
+      "https://ue8ehrrurhrurhrururu.vercel.app/api/status",
+
+      {
+        method:"POST",
+
+        headers:{
+          "Content-Type":
+          "application/json"
+        },
+
+        body:JSON.stringify({
+          mode:"paid"
+        })
+
+      }
+
+    );
+
+  }
+
+  // STOP
+  if(
+    text.toLowerCase() ===
+    "stop"
+  ){
+
+    await fetch(
+
+      "https://ue8ehrrurhrurhrururu.vercel.app/api/status",
+
+      {
+        method:"POST",
+
+        headers:{
+          "Content-Type":
+          "application/json"
+        },
+
+        body:JSON.stringify({
+          mode:"waiting"
+        })
+
+      }
+
+    );
+
+  }
+
+  res.status(200).send("ok");
+
+}
