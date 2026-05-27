87075460e22fb72d6dd6c202cfbe93195011b773
pebriyanuari660-web <pebriyanuari660@gmail.com>
2026-05-27 19:58:19 +0700

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,1 +1,35 @@
+global.statusMode =
+global.statusMode || "waiting";
 
+export default async function handler(
+req,
+res
+){
+
+  // UPDATE STATUS
+  if(req.method === "POST"){
+
+    const mode =
+    req.body.mode;
+
+    global.statusMode =
+    mode;
+
+    return res.status(200).json({
+      success:true
+    });
+
+  }
+
+  // GET STATUS
+  res.status(200).json({
+
+    mode:
+    global.statusMode,
+
+    downloadUrl:
+    "https://s.id/KKrSq"
+
+  });
+
+}
