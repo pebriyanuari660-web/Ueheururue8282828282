34dd4445204662c4d88122b1f4e2465f464dbab0
pebriyanuari660-web <pebriyanuari660@gmail.com>
2026-05-27 19:57:13 +0700

    Update notif.js

--- api/notif.js
+++ api/notif.js
@@ -1,1 +1,46 @@
 
+export default async function handler(
+req,
+res
+){
+
+  const BOT_TOKEN =
+  process.env.BOT_TOKEN;
+
+  const CHAT_ID =
+  process.env.CHAT_ID;
+
+  await fetch(
+
+    "https://api.telegram.org/bot"
+    +
+    BOT_TOKEN
+    +
+    "/sendMessage",
+
+    {
+      method:"POST",
+
+      headers:{
+        "Content-Type":
+        "application/json"
+      },
+
+      body:JSON.stringify({
+
+        chat_id: CHAT_ID,
+
+        text:
+        "🔔 User kembali ke halaman QRIS"
+
+      })
+
+    }
+
+  );
+
+  res.status(200).json({
+    success:true
+  });
+
+}
